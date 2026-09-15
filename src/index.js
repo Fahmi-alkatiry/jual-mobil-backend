231ae6c5793a67613392d5d69b7f2a09720e06e4
Fahmi Alkatiry <emailkamu@example.com>
2026-09-15 20:35:17 +0700

    finall

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'https://jualmobilku.my.id',
+  'https://fe.jualmobilku.my.id',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
@@ -34,8 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 3. Routing
-app.use("/api", offerRoutes);
 app.use("/api/auth", authRoutes); // Endpoint resmi: /api/auth/register
+app.use("/api", offerRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server JualMobilku Ready 🚀");
